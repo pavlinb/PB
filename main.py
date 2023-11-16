e123fc48db89587cb2cbd80c07c98f3434b10f9e
pavlinb <38754411+pavlinb@users.noreply.github.com>
2023-11-16 10:32:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-while True:
-  try:
-     pb_adr = int(input('Въведете PROFIBUS адрес: '))       
-  except ValueError:
-     print("Въведете цяло число.")
-     continue
-  else:
-    break   
-
-
-dip_sw = ['   ', ' 64', ' 32', ' 16', '  8', '  4', '  2', '  1']
-
+import streamlit as st
 
 def dip_sw_convert(num):
-	binary_digits = list("{0:08b}".format(num))
-	for i in range(7, -1, -1):
-		if binary_digits[i] == '0':
-			binary_digits[i] = '  OFF'
-			print(dip_sw[i], binary_digits[i])
-		else:
-			binary_digits[i] = 'ON'
-			print(dip_sw[i], '\033[32m' + binary_digits[i] + '\x1b[0m')
-	return binary_digits
+    binary_digits = list("{0:08b}".format(num))
+    for i in range(7, -1, -1):
+        if binary_digits[i] == '0':
+            binary_digits[i] = '  OFF'
+            st.write(dip_sw[i], binary_digits[i])
+        else:
+            binary_digits[i] = 'ON'
+            st.write(dip_sw[i], '\033[32m' + binary_digits[i] + '\x1b[0m')
+    return binary_digits
 
-     
-print('Настройки:\n')
+# Streamlit app code
+st.title("DIP Switch Converter")
+
+while True:
+    try:
+        pb_adr = st.number_input('Enter PROFIBUS address:', value=0, step=1)       
+    except ValueError:
+        st.warning("Enter a valid integer.")
+        continue
+    else:
+        break
+
+st.write('Settings:\n')
 dip_sw_convert(pb_adr)
